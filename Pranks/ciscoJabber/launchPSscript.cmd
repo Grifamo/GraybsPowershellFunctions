af5488e1669b1c5b845f7f67ca0341fcfd1cd6b3
Grifamo <32716390+Grifamo@users.noreply.github.com>
2021-03-31 08:03:08 -0500

    Delete Pranks/ciscoJabber directory

--- Pranks/ciscoJabber/launchPSscript.cmd
+++
@@ -1,2 +0,0 @@
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1 
-PowerShell C:\Users\Administrator\hidden_scripts\createCallMatt.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
